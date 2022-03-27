b3974abcaa863ede0fbd8f86d29583aaef080817
hyeyoom <neoul_chw@icloud.com>
2022-03-28 02:51:38 +0900

    test mock

---
+++ src/main/java/place/moim/api/TestMessage.java
@@ -0,0 +1,20 @@
+package place.moim.api;
+
+public class TestMessage {
+    private String message;
+
+    public TestMessage() {
+    }
+
+    public TestMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
