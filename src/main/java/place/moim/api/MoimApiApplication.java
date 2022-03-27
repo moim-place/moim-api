56c91ff4de2c0651e464b840ba0c53b578c78fe7
hyeyoom <neoul_chw@icloud.com>
2022-03-27 23:08:31 +0900

    initial project

---
+++ src/main/java/place/moim/api/MoimApiApplication.java
@@ -0,0 +1,13 @@
+package place.moim.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoimApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoimApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/place/moim/api/MoimApiApplicationTests.java
@@ -0,0 +1,13 @@
+package place.moim.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoimApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
